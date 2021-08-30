ea632f91e55690dbc5f7ddc73937a0cb722c5b4e
sangkipark <sangki931@gmail.com>
2021-08-30 22:20:28 +0900

    commit Week05

---
+++ WeeklyChallenge/src/Week05/Solution.java
@@ -0,0 +1,9 @@
+package Week05;
+
+class Solution {
+    public int solution(String word) {
+        int answer = 0, per = 3905;
+        for(String s : word.split("")) answer += "AEIOU".indexOf(s) * (per /= 5) + 1;
+        return answer;
+    }
+}
