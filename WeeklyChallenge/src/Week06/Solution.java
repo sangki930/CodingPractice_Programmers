02412a008b156a0f4b3ed65140b11626651f54af
sangkipark <sangki931@gmail.com>
2021-09-08 22:53:16 +0900

    commit Week06

---
+++ WeeklyChallenge/src/Week06/Solution.java
@@ -0,0 +1,74 @@
+package Week06;
+
+import java.util.*;
+
+class Boxer{
+    int id;
+    int weight;
+    int v;// ÀÚ±âº¸´Ù ¹«°Å¿î º¹¼­¸¦ ÀÌ±ä È½¼ö
+    int plays;//°æ±â È½¼ö
+    int victory;// ÀÌ±ä È½¼ö
+    double rate;//½Â·ü
+    public Boxer(int id, int weight, int v, int victory, int plays){
+        this.id=id;
+        this.weight=weight;
+        this.v=v;
+        this.victory=victory;
+        this.plays=plays;
+        this.rate=plays==0?0.0:(double)victory/(double)plays;
+    }
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        return sb.append("[ ").append("id").append(" : ").append(this.id).append(", ")
+                 .append("weight").append(" : ").append(this.weight).append(", ")
+                 .append("v").append(" : ").append(this.v).append(", ")
+                 .append("victory").append(" : ").append(this.victory).append(", ")
+                 .append("plays").append(" : ").append(this.plays).append(", ")
+                 .append("rate").append(" : ").append(this.rate).append(" ]").toString();
+    }
+}
+
+class Solution {
+    public int[] solution(int[] weights, String[] head2head) {
+        int n = weights.length;
+        int[] answer = new int[n];
+        List<Boxer> list = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            int none = 0;
+            int v = 0;
+            int victory = 0;
+            int defeat = 0;
+            int id=i+1;
+            int weight=weights[i];
+            for(int j=0;j<head2head[i].length();j++){
+                char c = head2head[i].charAt(j);
+                switch(c){
+                    case 'W'->{
+                        victory++;
+                        if(weight<weights[j]) v++;
+                    }
+                    case 'L'->defeat++;
+                    case 'N'->none++;
+                }
+            }
+            list.add(new Boxer(id,weight,v,victory,victory+defeat));
+        }
+        
+        list.sort((a,b)->{
+            if(a.rate==b.rate){
+                if(a.v==b.v){
+                    if(a.weight==b.weight)
+                        return Integer.compare(a.id,b.id);
+                    return Integer.compare(b.weight,a.weight);
+                }
+                return Integer.compare(b.v,a.v);
+            }
+            return Double.compare(b.rate,a.rate);
+        });
+        for(int i=0;i<list.size();i++){
+            answer[i]=list.get(i).id;
+        }
+        
+        return answer;
+    }
+}
