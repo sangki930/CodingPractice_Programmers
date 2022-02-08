879c18874aeac472691caaa078ee013c0ff19a7b
sangkipark <sangki931@gmail.com>
2022-02-08 22:29:12 +0900

    commit 완주하지 못한 선수

---
+++ 고득점KIT/src/hash/완주하지_못한_선수/Solution.java
@@ -0,0 +1,22 @@
+package hash.완주하지_못한_선수;
+
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+   
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
+        for (String player : completion) hm.put(player, hm.get(player) - 1);
+
+        for (String key : hm.keySet()) {
+            if (hm.get(key) != 0){
+                answer = key;
+            }
+        }
+        return answer;
+      
+    }
+    
+}
