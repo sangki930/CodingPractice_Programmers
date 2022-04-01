187fca8e8c16041c5cd1c25124d4c541fcaf42e6
sangkipark <sangki931@gmail.com>
2022-04-01 23:47:34 +0900

    commit

--- 고득점KIT/src/sort/K번째수/Solution.java
+++ 고득점KIT/src/sort/K번째수/Solution.java
@@ -6,8 +6,6 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         
-        // System.out.println("array :" + Arrays.toString(array));
-        
         for(int i=0;i<commands.length;i++){
              answer[i]
                  =resultsort(commands[i][0],commands[i][1],commands[i][2],array);
@@ -19,20 +17,12 @@ class Solution {
     public int resultsort(int i,int j,int k,int[] array){
         int[] arrnum = new int[(j-i)+1];
         int index=0;
-        // System.out.println(j-i+1);
-        // System.out.println("i값 : "+i+" j값 : "+j);
-        // System.out.println("메소드 내의 array : "+Arrays.toString(array));
         for(int a=i-1;a<=j-1;a++){
-            // System.out.println("index 값 : " + index);
-            // System.out.println("array 값 : "+array[a]);
             arrnum[index] = array[a];
             index++;
-            
-            // System.out.println("현재 Array : "+ Arrays.toString(arrnum));
         }
         
         Arrays.sort(arrnum);
-        // System.out.println(Arrays.toString(arrnum));
         return arrnum[k-1];
     }
 }
