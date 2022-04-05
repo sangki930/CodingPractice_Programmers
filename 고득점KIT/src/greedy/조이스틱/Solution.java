f9b6aedaba8d89fbec4f0dfb73c67305d2dbf65a
sangkipark <sangki931@gmail.com>
2022-04-05 23:59:07 +0900

    commit

--- 고득점KIT/src/greedy/조이스틱/Solution.java
+++ 고득점KIT/src/greedy/조이스틱/Solution.java
@@ -6,7 +6,6 @@ class Solution {
         int[] diff={0,1,2,3,4,5,6,7,8,9,10,11,12,13,12,11,10,9,8,7,6,5,4,3,2,1};
         for(char c:name.toCharArray())
             answer+=diff[c-'A'];
-
         int length=name.length();
         int min=length-1;
 
