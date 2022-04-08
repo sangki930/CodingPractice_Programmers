ee0f732d5cf936655f6417dcd88a53a0653e550e
sangkipark <sangki931@gmail.com>
2022-04-08 22:54:05 +0900

    commit 구명보트

---
+++ 고득점KIT/src/greedy/구명보트/Solution.java
@@ -0,0 +1,17 @@
+package greedy.구명보트;
+
+import java.util.*;
+
+class Solution {
+    public static int solution(int[] people, int limit) {
+        Arrays.sort(people);
+ 
+        int answer = 0;
+        int index = people.length - 1;
+        for(int i = 0; i <= index; i++, answer++) 
+            while(index > i && people[i] + people[index--] > limit) 
+                answer++;
+ 
+        return answer;
+    }
+}
