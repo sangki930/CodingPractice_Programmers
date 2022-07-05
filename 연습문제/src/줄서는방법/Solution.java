45ffa450f524bdde8b88f4d7ef6da26a17c2d5c8
sangkipark <sangki931@gmail.com>
2022-07-05 22:15:49 +0900

    commit 줄 서는 방법

---
+++ 연습문제/src/줄서는방법/Solution.java
@@ -0,0 +1,28 @@
+package 줄서는방법;
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, long k) {
+        int[] answer = new int[n];
+        List<Integer> list = new ArrayList<>();
+        long num = 1;
+        int i=1;
+        for(i=1;i<=n;i++){
+            list.add(i);
+            num*=i;
+        }
+        i=0;
+        long mod = k-1;
+        
+        while(i<n){
+            num /= (n-i);
+            int value = (int)(mod / num);
+            answer[i++] = list.get(value);
+            list.remove(value);
+            mod %= num;
+        }
+        
+        return answer;
+    }
+}
