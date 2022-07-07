de4b3c365fe030cddee4e1303630daedba097c9c
sangkipark <sangki931@gmail.com>
2022-07-07 23:54:43 +0900

    commit 폰켓몬

---
+++ 찾아라프로그래밍마에스터/src/폰켓몬/Solution.java
@@ -0,0 +1,16 @@
+package ÆùÄÏ¸ó;
+
+import java.util.*;
+import java.util.stream.*;
+class Solution {
+    public int solution(int[] nums) {
+	        int answer = 0;
+	        
+	        Set<Integer> set =  Arrays.stream(nums)
+	        		.boxed()
+	        		.collect(Collectors.toSet());
+
+	        return answer = set.size()>nums.length/2?nums.length/2:set.size();
+	    }
+
+}
