131efd851bc1f15d33953be578091c999f51f3a7
sangkipark <sangki931@gmail.com>
2022-07-24 23:03:55 +0900

    commit

---
+++ 연습문제/src/_2xN타일링/Solution.java
@@ -0,0 +1,26 @@
+package _2xN≈∏¿œ∏µ;
+
+import java.util.*;
+
+class Solution {
+    
+    static int dp[]=new int[60001];
+    public int cal(int n){
+        
+        if(n==1){
+            dp[n]=1;
+            return 1;
+        }
+        else if(n==2){
+            dp[n]=2;
+            return 2;
+        }
+        if(dp[n]!=0)
+            return dp[n];
+        return dp[n]=(cal(n-1)+cal(n-2))%1000000007;
+        
+    }
+    public int solution(int n) {
+        return cal(n);
+    }
+}
