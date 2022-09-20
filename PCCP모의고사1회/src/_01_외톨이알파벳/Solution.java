67395a710b58e92f703cd590ef6e6a5b11e75e77
sangkipark <sangki931@gmail.com>
2022-09-21 00:02:07 +0900

    commit 외톨이 알파벳

--- PCCP모의고사1회/src/_01_외톨이알파벳/Solution.java
+++ PCCP모의고사1회/src/_01_외톨이알파벳/Solution.java
@@ -1,5 +1,37 @@
 package _01_외톨이알파벳;
 
-public class Solution {
+import java.util.*;
 
+class Solution {
+    public String solution(String input_string) {
+        String answer = "";
+        Map<Character,ArrayList<String>> map = new TreeMap<>();
+        
+        String s = input_string.charAt(0)+"";
+        char c = '6';
+        for(int i=0;i<input_string.length()-1;i++){
+            c = input_string.charAt(i);
+            if(c==input_string.charAt(i+1)){
+                s+=input_string.charAt(i+1);
+            }else{
+                ArrayList<String> arr = map.getOrDefault(c,new ArrayList<>());
+                arr.add(s);
+                map.put(c,arr);
+                s = input_string.charAt(i+1)+"";
+                c = input_string.charAt(i+1);
+            }
+        }
+        
+        ArrayList<String> arr1 = map.getOrDefault(c,new ArrayList<>());
+        arr1.add(s);
+        map.put(c,arr1);
+        
+        for(char ch : map.keySet()){
+            ArrayList<String> arr = map.getOrDefault(ch,new ArrayList<>());
+            if(arr.size()>=2)
+                answer+=ch;
+        }
+        if(answer.isBlank()) return "N";
+        return answer;
+    }
 }
