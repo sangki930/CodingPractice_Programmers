76b0e22aedf5ec8e1ba15863357906588c4ab151
sangki930 <sangki931@gmail.com>
2022-12-27 14:10:25 +0900

    commit 디펜스게임

---
+++ 연습문제/src/디펜스게임/Solution.java
@@ -0,0 +1,34 @@
+package 디펜스게임;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int k, int[] enemy) {
+        int answer = enemy.length;
+        int len = enemy.length;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int sum = 0;
+        for(int i=0;i<len;i++){
+            int v = enemy[i];
+            if(pq.size()<k){
+                pq.offer(v);
+            }else if(pq.size()==k){
+                int num = pq.peek();
+                if(num<v){
+                    int tmp = pq.poll();
+                    sum+=tmp;
+                    pq.offer(v);
+                }else{
+                    sum+=v;
+                }
+            }
+            if(sum>n){
+                answer = i;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
