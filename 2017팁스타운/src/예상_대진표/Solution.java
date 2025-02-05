f43349857f3d2437d4c8dcbe347ed7b2c82b1f35
sangki930 <sangki931@gmail.com>
2025-02-05 23:56:17 +0900

    commit

--- 2017팁스타운/src/예상_대진표/Solution.java
+++ 2017팁스타운/src/예상_대진표/Solution.java
@@ -3,12 +3,13 @@ package 예상_대진표;
 import java.util.*;
 
 class Solution {
+
     public int solution(String[] strs, String t) {
         int answer = 0;
         int n = t.length();
         Set<String> set = Set.of(strs);
         int dp[]=new int[n+1];
-        
+
         for(int i=1;i<n+1;i++){
             dp[i]=20001;
             int s=0;
@@ -21,7 +22,8 @@ class Solution {
         }
         if(dp[dp.length-1]==20001)
             return -1;
-        
+
         return dp[dp.length-1];
     }
+
 }
